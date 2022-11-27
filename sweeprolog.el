f4e9d872c81bab6772025f2a481adbc25338f37f
Eshel Yaron <me@eshelyaron.com>
2022-11-27 21:44:52 +0200

    Announce recent changes in NEWS.org and bump version to 0.9.3

--- sweeprolog.el
+++ sweeprolog.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Eshel Yaron <~eshel/dev@lists.sr.ht>
 ;; Keywords: prolog languages extensions
 ;; URL: https://git.sr.ht/~eshel/sweep
-;; Package-Version: 0.9.2
+;; Package-Version: 0.9.3
 ;; Package-Requires: ((emacs "28.1"))
 
 ;; This file is NOT part of GNU Emacs.
